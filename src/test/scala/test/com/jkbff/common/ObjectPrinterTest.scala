0296cb0c92dc9eeef81549ade8a0c59fcbaa447a
Jason Wheeler <email1@jkbff.com>
2014-05-07 15:09:41 -0600

    added support for maven packaging

--- src/test/scala/test/com/jkbff/common/ObjectPrinterTest.scala
+++ src/test/scala/test/com/jkbff/common/ObjectPrinterTest.scala
@@ -11,21 +11,21 @@ class ObjectPrinterTest extends FunSuite {
 	test("getFields()") {
 		val objectPrinter = new ObjectPrinter("  ")
 
-		val obj = new TestClass2("hi", "bye1")
-		
+		val obj = new SomeObject2("hi", "bye1")
+
 		val expected = List("nest1", "nest2", "intVal", "longInt", "dub", "floatingPoint", "shortVal", "byteVal", "charVal", "someList", "someSet", "someMap", "hello", "goodBye")
-		
+
 		assert(objectPrinter.getFields(obj).map(x => x.getName()).toList == expected)
 	}
-	
+
 	test("getClassHierarchy()") {
 		val objectPrinter = new ObjectPrinter("  ")
 
-		val testClass = new TestClass2("hi", "bye1")
-		
-		val expected = List(classOf[TestClass2], classOf[TestClass])
+		val testClass = new SomeObject2("hi", "bye1")
+
+		val expected = List(classOf[SomeObject2], classOf[SomeObject])
 		val result = objectPrinter.getClassHierarchy(testClass.getClass()).takeWhile(_.getName() != "java.lang.Object").toList
-		
+
 		assert(result == expected)
 	}
 
@@ -34,33 +34,33 @@ class ObjectPrinterTest extends FunSuite {
 
 		val customPrinter = new SomeCustomPrinter(testPrinter)
 
-		testPrinter.addCustom(classOf[Test1], customPrinter)
+		testPrinter.addCustom(classOf[SomeObject3], customPrinter)
 
-		assert(testPrinter.checkForCustomPrinter(classOf[Test1]).get._2 == customPrinter)
-		assert(testPrinter.checkForCustomPrinter(classOf[Test2]).get._2 == customPrinter)
-		assert(testPrinter.checkForCustomPrinter(classOf[Test3]).get._2 == customPrinter)
-		assert(testPrinter.checkForCustomPrinter(classOf[Test4]).isEmpty)
+		assert(testPrinter.checkForCustomPrinter(classOf[SomeObject3]).get._2 == customPrinter)
+		assert(testPrinter.checkForCustomPrinter(classOf[SomeObject4]).get._2 == customPrinter)
+		assert(testPrinter.checkForCustomPrinter(classOf[SomeObject5]).get._2 == customPrinter)
+		assert(testPrinter.checkForCustomPrinter(classOf[SomeObject6]).isEmpty)
 	}
 
 	test("printObj for a string") {
 		val objectPrinter = new ObjectPrinter("  ")
 
 		val expected = "obj: String = hi\n"
-		
+
 		assert(objectPrinter.printObj("hi", true) == expected)
 	}
-	
+
 	test("printObj for an object with no fields") {
 		val objectPrinter = new ObjectPrinter("  ")
 
 		val testclass = new AnyRef
-		
+
 		val expected = "obj = \n  <no fields>\n"
 		val result = objectPrinter.printObj(testclass, false)
-		
+
 		assert(result == expected)
 	}
-	
+
 	test("printObj for a java Collection") {
 		val objectPrinter = new ObjectPrinter("  ")
 
@@ -68,12 +68,12 @@ class ObjectPrinterTest extends FunSuite {
 		list.add("hello")
 		list.add("hey")
 		list.add("hi")
-		
+
 		val expected = "obj: java.util.ArrayList = \n  [0]: String = hello\n  [1]: String = hey\n  [2]: String = hi\n"
-		
+
 		assert(objectPrinter.printObj(list, true) == expected)
 	}
-	
+
 	test("printObj for a java map") {
 		val objectPrinter = new ObjectPrinter("  ")
 
@@ -81,24 +81,25 @@ class ObjectPrinterTest extends FunSuite {
 		map.put("first", "a1")
 		map.put("second", "a2")
 		map.put("third", "a3")
-		
+
 		val expected = "obj: java.util.HashMap = \n  [second]: String = a2\n  [third]: String = a3\n  [first]: String = a1\n"
-		
+
 		assert(objectPrinter.printObj(map, true) == expected)
 	}
-	
+
 	test("printObj for a complex object") {
 		val objectPrinter = new ObjectPrinter("  ")
 
-		val expected = "obj: test.com.jkbff.common.TestClass2 = \n  nest1: test.com.jkbff.common.TestClass = \n    hello: String = hola\n    goodBye: String = adios\n  nest2: test.com.jkbff.common.TestClass = \n    hello: String = bonjour!\n    goodBye: String = adieu\n  intVal: Integer = 987\n  longInt: Long = 1234\n  dub: Double = 12.34\n  floatingPoint: Float = 123.456\n  shortVal: Short = 123\n  byteVal: Byte = 111\n  charVal: Character = J\n  someList: scala.collection.immutable.$colon$colon = \n    [0]: Integer = 1\n    [1]: Integer = 2\n    [2]: Integer = 3\n  someSet: scala.collection.immutable.Set$Set3 = \n    [0]: String = a\n    [1]: String = b\n    [2]: String = c\n  someMap: scala.collection.immutable.HashMap$HashTrieMap = \n    [cc]: String = jamie\n    [aa]: String = jason\n    [bb]: String = jodie\n    [ee]: String = casey\n    [ff]: String = cody\n    [dd]: String = kelsie\n  hello: String = hi\n  goodBye: String = bye1\n"
-		
-		assert(objectPrinter.printObj(new TestClass2("hi", "bye1"), true) == expected)
+		val expected = "obj: test.com.jkbff.common.SomeObject2 = \n  nest1: test.com.jkbff.common.SomeObject = \n    hello: String = hola\n    goodBye: String = adios\n  nest2: test.com.jkbff.common.SomeObject = \n    hello: String = bonjour!\n    goodBye: String = adieu\n  intVal: Integer = 987\n  longInt: Long = 1234\n  dub: Double = 12.34\n  floatingPoint: Float = 123.456\n  shortVal: Short = 123\n  byteVal: Byte = 111\n  charVal: Character = J\n  someList: scala.collection.immutable.$colon$colon = \n    [0]: Integer = 1\n    [1]: Integer = 2\n    [2]: Integer = 3\n  someSet: scala.collection.immutable.Set$Set3 = \n    [0]: String = a\n    [1]: String = b\n    [2]: String = c\n  someMap: scala.collection.immutable.HashMap$HashTrieMap = \n    [cc]: String = jamie\n    [aa]: String = jason\n    [bb]: String = jodie\n    [ee]: String = casey\n    [ff]: String = cody\n    [dd]: String = kelsie\n  hello: String = hi\n  goodBye: String = bye1\n"
+		val result = objectPrinter.printObj(new SomeObject2("hi", "bye1"), true)
+
+		assert(result == expected)
 	}
 
 	test("printObj with null values") {
 		val objectPrinter = new ObjectPrinter("  ")
 
-		val obj = new Test4
+		val obj = new SomeObject6
 
 		val expected = "obj = \n  someRef = null\n"
 		val result = objectPrinter.printObj(obj, false)
@@ -141,22 +142,22 @@ class ObjectPrinterTest extends FunSuite {
 	test("printObj with a custom printer") {
 		val testPrinter = new ObjectPrinter("  ")
 
-		testPrinter.addCustom(classOf[Test1], new SomeCustomPrinter(testPrinter))
+		testPrinter.addCustom(classOf[SomeObject3], new SomeCustomPrinter(testPrinter))
 
-		val expected = "obj = \n  test1: class test.com.jkbff.common.Test3\n"
-		val result = testPrinter.printObj(new Test3, false)
+		val expected = "obj = \n  test1: class test.com.jkbff.common.SomeObject5\n"
+		val result = testPrinter.printObj(new SomeObject5, false)
 
 		assert(result == expected)
 	}
 }
 
-class TestClass(val hello: String, val goodBye: String) {
-	
+class SomeObject(val hello: String, val goodBye: String) {
+
 }
 
-class TestClass2(hello: String, goodBye: String) extends TestClass(hello, goodBye) {
-	val nest1 = new TestClass("hola", "adios")
-	val nest2 = new TestClass("bonjour!", "adieu")
+class SomeObject2(hello: String, goodBye: String) extends SomeObject(hello, goodBye) {
+	val nest1 = new SomeObject("hola", "adios")
+	val nest2 = new SomeObject("bonjour!", "adieu")
 	val intVal = 987
 	val longInt = 1234L
 	val dub = 12.34
@@ -169,17 +170,17 @@ class TestClass2(hello: String, goodBye: String) extends TestClass(hello, goodBy
 	val someMap = Map("aa" -> "jason", "bb" -> "jodie", "cc" -> "jamie", "dd" -> "kelsie", "ee" -> "casey", "ff" -> "cody")
 }
 
-class Test1 {
+class SomeObject3 {
 
 }
 
-class Test2 extends Test1 {
+class SomeObject4 extends SomeObject3 {
 
 }
 
-class Test3 extends Test2
+class SomeObject5 extends SomeObject4
 
-class Test4 {
+class SomeObject6 {
 	val someRef: Any = null
 }
 
